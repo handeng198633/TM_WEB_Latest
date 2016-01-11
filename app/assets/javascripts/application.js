69c535ada70974b5251ec2aff47bcb00caa73262
Deng Han <han.deng@ansys.com>
2016-01-11 23:34:25 +0800

    AdminLTE and datatable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,27 +10,13 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require prng4
-//= require jsbn
-//= require m
-//= require functions
-//= require base64
-//= require rng
-//= require rsa
-//= require handlebars-1.0.0.beta.6
-//= require jquery-1.11.1.min
-//= require jquery-migrate-1.2.1
-//= require jquery.easyui.min
-//= require jquery.uploadify
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require jquery-ui
-//= require bootstrap
 //= require turbolinks
-//= require jquery.dataTables.min
-
 
 //= require packageorders
 //= require fitorders
--- app/assets/javascripts/packageorders.js.coffee
+++ app/assets/javascripts/packageorders.js.coffee
@@ -1,7 +1,11 @@
-jQuery ->
-  $('#packageorders').dataTable
-    sPaginationType: "full_numbers"
-    bJQueryUI: true
-    bProcessing: true
-    bServerSide: true
-    sAjaxSource: $('#packageorders').data('source')
+$('#packageorders').DataTable({
+  // sAjaxSource: $('#packageorders').data('source'),
+  // autoWidth: false,
+  // pagingType: 'full_numbers',
+  // processing: true,
+  // serverSide: true,
+
+  // Optional, if you want full pagination controls.
+  // Check dataTables documentation to learn more about available options.
+  // http://datatables.net/reference/option/pagingType
+});
